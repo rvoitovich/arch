285f892a434649354dbb5e1601b005c3ffb62209
rvoitovich <51600304+rvoitovich@users.noreply.github.com>
2022-03-16 23:09:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,10 @@ user="user"
 password="password"
 
 devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
-device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
-clear
+
+echo devicelist
+echo -n "Select installation disk: "
+read -s device
 
 timedatectl set-ntp true
 
